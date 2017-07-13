ab45d0071e9d4badac99d66a70095c4ae2ed02c8
Jason Miller <jason@jasonpmiller.com>
2017-07-12 21:39:59 -0700

    refactor var names and added syllable tests

--- test/mechanics/syllabify.spec.js
+++ test/mechanics/syllabify.spec.js
@@ -1,12 +1,19 @@
 var expect = require('expect');
 var Syllabify = require('../../lib/mechanics/syllabify');
 
-describe('removeAccent', function() {
+describe('getUnits', function() {
 
-	it('should return an array of two syllables for the word λύω', function() {
-		var actual = Syllabify.getUnits('λύω');
+	it('should return the correct two syllables for the word λυω (without accent)', function() {
+		var actual = Syllabify.getUnits('λυω');
 
 		var expected = ['λυ', 'ω'];
+		expect(actual).toEqual(expected);
+	});
+
+	it('should return the correct four syllables for the word ἐλυόμην', function() {
+		var actual = Syllabify.getUnits('ελυομην');
+
+		var expected = ['ε', 'λυ', 'ο', 'μην'];
 		expect(actual).toEqual(expected);
 	});
 
